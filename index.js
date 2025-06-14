c0cf0fccd177f507935008bc9ea7e317c651e01e
Nicholas obonyo abala <nicholasobonyo444@gmail.com>
2025-06-14 13:02:40 +0300

    fully functional

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, driverName) {
+    return drivers.filter(driver => driver.toLowerCase() === driverName.toLowerCase());
+}
+
+function fuzzyMatch(drivers, startingLetters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(startingLetters.toLowerCase()));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
